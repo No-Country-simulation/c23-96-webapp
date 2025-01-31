8b5d6c39e481180f77f6011aab9334c15eb063d4
Moisarm <v.30247986.mr@gmail.com>
2025-01-30 20:18:51 -0400

    fix in .populate

--- server/src/controllers/admin/adminHistory.controller.js
+++ server/src/controllers/admin/adminHistory.controller.js
@@ -2,18 +2,17 @@ const transactionModel = require("../../models/transaction");
 
 module.exports.history = async (req, res, next) => {
   try {
-    // Obtener todas las transacciones
     const transactions = await transactionModel
       .find()
       .populate({
         path: "originccount",
         select: "account cvu balancePeso balanceDolar",
-      }) // 🔹 Datos de la cuenta de origen
+      })
       .populate({
         path: "destinationAccount",
         select: "account cvu balancePeso balanceDolar",
-      }) // 🔹 Datos de la cuenta destino
-      .sort({ date: -1 }); // 🔹 Orden descendente por fecha
+      })
+      .sort({ date: -1 });
 
     res.status(200).json({
       message: "Historial de transacciones obtenido con éxito.",
