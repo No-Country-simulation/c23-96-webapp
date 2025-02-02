3d6d6a9d958a2f5521a81da42335c8de718dd932
Moisarm <v.30247986.mr@gmail.com>
2025-02-02 04:14:11 -0400

    using http-errors

--- server/src/controllers/admin/offer/updateOffer.controller.js
+++ server/src/controllers/admin/offer/updateOffer.controller.js
@@ -1,24 +1,30 @@
 const offerModel = require("../../../models/offer");
 const userModel = require("../../../models/user");
-
+const createHttpError = require("http-errors");
 module.exports.updateOffer = async (req, res, next) => {
   try {
     const { id } = req.params;
+
     const offer = await offerModel.findById(id);
+
     if (!offer) {
-      return res.status(404).json({ message: "Not found" });
+      throw createHttpError(404, "Not Found.");
     }
+
     const { title, description, discountPercentage, expirationDate } = req.body;
     const user = await userModel.findById(req.user._id);
+
     if (!user) {
-      return res.status(401).json({ message: "Unauthorized" });
+      throw createHttpError(401, "Unauthorized.");
     }
+
     offer.title = title;
     offer.description = description;
     offer.discountPercentage = discountPercentage;
     offer.expirationDate = expirationDate;
     offer.updatedBy = user._id;
     await offer.save();
+
     return res.status(200).json({ message: "Offeer updated", offer });
   } catch (error) {
     next(error);
