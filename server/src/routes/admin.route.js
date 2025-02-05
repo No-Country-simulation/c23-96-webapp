773eb9b7a854f05e3986a23153a99eeb4c04a156
Moisarm <v.30247986.mr@gmail.com>
2025-02-05 17:28:58 -0400

    change controller in route and delete controller

--- server/src/routes/admin.route.js
+++ server/src/routes/admin.route.js
@@ -3,17 +3,22 @@ const { listUsers } = require("../controllers/admin/listUsers.controller");
 const { history } = require("../controllers/admin/adminHistory.controller");
 const { verifyToken } = require("../middlewares/verifyToken.middleware");
 const { newOffer } = require("../controllers/admin/offer/newOffer.controller");
-const { getOffers } = require("../controllers/admin/offer/getOffers.controller");
-const { updateOffer } = require("../controllers/admin/offer/updateOffer.controller");
-
+const {
+  getOffers,
+} = require("../controllers/admin/offer/getOffers.controller");
+const {
+  updateOffer,
+} = require("../controllers/admin/offer/updateOffer.controller");
+const {
+  deleteOffer,
+} = require("../controllers/admin/offer/deleteOffers.controller");
 const adminRouter = express.Router();
 
 adminRouter.get("/", verifyToken, listUsers);
 adminRouter.get("/history", history);
 adminRouter.post("/offer", newOffer);
 adminRouter.get("/offer", getOffers);
-adminRouter.delete("/offer/:id", newOffer);
-adminRouter.put("/offer/:id", updateOffer)
-
+adminRouter.delete("/offer/:id", deleteOffer);
+adminRouter.put("/offer/:id", updateOffer);
 
 module.exports = adminRouter;
